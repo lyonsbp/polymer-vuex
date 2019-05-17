77029d3819df81b7b8238488a4e8f3eb925068f1
Brandon Lyons <lyonsbp@umich.edu>
2019-05-17 10:54:14 -0400

    Add built version

---
+++ build/default/main.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const store = new Vuex.Store({
+    state: {
+      message: "From the vuex store",
+      count: 0
+    },
+    mutations: {
+      updateText(state, payload) {
+        state.message = payload;
+      },
+
+      addOne(state) {
+        state.count++;
+      }
+
+    }
+  });
+  store.subscribe((mutation, state) => {
+    window.dispatchEvent(new Event(`vuex-${mutation.type}`));
+  });
+  let vm = new Vue({
+    el: '#vue-app',
+    data: {
+      initialText: "Hello from Vue"
+    },
+    computed: {
+      message() {
+        return this.$store.state.message;
+      },
+
+      count() {
+        return this.$store.state.count;
+      }
+
+    },
+    store
+  });
+  window.vm = vm;
+});
